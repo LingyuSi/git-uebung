80dbd6b9e276ba79a62436342e43c27d76d9fb3b
changsi <sichang1113@gmail.com>
2025-10-30 12:18:14 +0100

    Add multiplication function (mul)

--- rechner.c
+++ rechner.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 int add(int a, int b) { return a + b; }
+int mul(int a, int b) { return a * b; }
 
 int main() {
-    int x = 2, y = 3;
+    int x = 5, y = 7;
     printf("Summe: %d\n", add(x, y));
+    printf("Produkt: %d\n", mul(x, y));
     return 0;
 }
